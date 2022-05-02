421646e9925dc5d574fc96fc68eecff9a3758d1d
Pablo Vera <pablo.vera82@gmail.com>
2022-05-02 01:51:22 -0400

    Add form to quoter app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header
-      titulo="Insurance Quoter"
-    />
+    <Contenedor>
+      <Header
+        titulo="Insurance Quoter"
+      />
+
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
