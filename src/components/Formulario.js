6600c47ca3b7ffdf8074131f33249c90420eca70
Pablo Vera <pablo.vera82@gmail.com>
2022-05-02 20:16:17 -0400

    Add logis to calculate taxes per year

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear } from '../helper';
 
 const Campo = styled.div`
     display: flex;
@@ -81,9 +82,14 @@ const Formulario = () => {
 
         guardarError(false);
 
-        // obtener la diferencia de años
+        let resultado = 2000;
 
+        // obtener la diferencia de años
+        const diferencia = obtenerDiferenciaYear(year);
+        
         // por cada año hay que restar el 3%
+        resultado -= (( diferencia * 3 ) * resultado) / 100;
+        console.log(resultado);
 
         // Americano 15
         // Asiatico 5%
@@ -125,6 +131,8 @@ const Formulario = () => {
                     onChange={obtenerInformacion}
                 >
                     <option value="">-- Seleccione --</option>
+                    <option value="2023">2023</option>
+                    <option value="2022">2022</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
                     <option value="2019">2019</option>
